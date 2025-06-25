92f505b279478e301a1de88a8fcc67aa2542b408
i-shashanksharma <shashanksharma240602005@gmail.com>
2025-06-25 13:35:40 +0530

    ASSIGNMENT

---
+++ Exer5.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const oldPara = document.getElementById("old-paragraph");
+  const newPara = document.createElement("p");
+  newPara.textContent = "This paragraph was replaced!";
+  oldPara.replaceWith(newPara);
+});
